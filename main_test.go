1c17609676ebea408470604a198544b5b69a9513
focus1024-wind <focus1024@foxmail.com>
2024-10-23 14:35:14 +0800

    PREF: 优化GB28181设备数据库搜索语句

--- main_test.go
+++ main_test.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-resty/resty/v2"
 	"testing"
 )
 
-type DefaultMediaResponse struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-}
-
-func GetMediaInvitePort() {
-	client := resty.New()
-
-	response := &struct {
-		Code int    `json:"code"`
-		Msg  string `json:"msg"`
-		Port int    `json:"port"`
-	}{}
-	resp, err := client.R().SetQueryParams(map[string]string{
-		"secret":    "Uva5PKPA1aGOoBiQHglYExH2kMi2cX2S",
-		"port":      "0",
-		"tcp_mode":  "1",
-		"vhost":     "__defaultVhost__",
-		"app":       "rtp",
-		"stream_id": "test",
-	}).SetResult(response).SetError(response).Get("http://localhost:80/index/api/openRtpServer")
-	fmt.Println(resp)
-	fmt.Println(err)
-	fmt.Println(response)
-}
-
 func Test(t *testing.T) {
-	GetMediaInvitePort()
+
 }
--- src/apps/ipc/ipc_device/server.go
+++ src/apps/ipc/ipc_device/server.go
@@ -7,24 +7,44 @@ import (
 	"fmt"
 )
 
-func (device *IpcDevice) DeviceOffline(string) {
-
-}
-
 func (device *IpcDevice) LoadDevice(deviceId string) (gb28181_server.GB28181Device, bool) {
 	var gb28181Device IpcDevice
 	err := global.Db.Where(&IpcDevice{DeviceID: deviceId}).First(&gb28181Device).Error
 	if err != nil {
-		return gb28181_server.GB28181Device{}, false
+		return gb28181_server.GB28181Device{DeviceID: deviceId}, false
 	} else {
 		gb28181Device.GB28181Device.DeviceID = gb28181Device.DeviceID
 		return gb28181Device.GB28181Device, true
 	}
 }
 
+func (device *IpcDevice) StoreDevice(gb28181Device gb28181_server.GB28181Device) {
+	err := global.Db.Debug().Where(&IpcDevice{
+		DeviceID: gb28181Device.DeviceID,
+	}).Save(&IpcDevice{
+		DeviceID:      gb28181Device.DeviceID,
+		GB28181Device: gb28181Device,
+	}).Error
+	if err != nil {
+		global.Db.Debug().Where(&IpcDevice{
+			DeviceID: gb28181Device.DeviceID,
+		}).Updates(&IpcDevice{
+			DeviceID:      gb28181Device.DeviceID,
+			GB28181Device: gb28181Device,
+		})
+	}
+}
+
+func (device *IpcDevice) DeviceOffline(deviceId string) {
+	var gb28181Device IpcDevice
+	global.Db.Where(&IpcDevice{DeviceID: deviceId}).First(&gb28181Device)
+	gb28181Device.Status = gb28181_server.DeviceOffLineStatus
+	global.Db.Save(&gb28181Device)
+}
+
 func (device *IpcDevice) LoadChannel(deviceId string, channelId string) (gb28181_server.GB28181Channel, bool) {
 	var channel IpcChannel
-	err := global.Db.Debug().Where(&IpcChannel{
+	err := global.Db.Where(&IpcChannel{
 		ParentID: deviceId,
 		DeviceID: channelId,
 	}).First(&channel).Error
@@ -40,7 +60,7 @@ func (device *IpcDevice) LoadChannel(deviceId string, channelId string) (gb28181
 
 func (device *IpcDevice) LoadChannels(deviceId string) ([]gb28181_server.GB28181Channel, bool) {
 	result := make([]IpcChannel, 0)
-	err := global.Db.Debug().Where(&IpcChannel{ParentID: deviceId}).Take(&result).Error
+	err := global.Db.Where(&IpcChannel{ParentID: deviceId}).Take(&result).Error
 	if err != nil {
 		return []gb28181_server.GB28181Channel{}, false
 	} else {
@@ -53,23 +73,6 @@ func (device *IpcDevice) LoadChannels(deviceId string) ([]gb28181_server.GB28181
 
 		return gb28181Channel, true
 	}
-}
-
-func (device *IpcDevice) StoreDevice(gb28181Device gb28181_server.GB28181Device) {
-	global.Db.Debug().Create(&IpcDevice{DeviceID: gb28181Device.DeviceID, GB28181Device: gb28181Device})
-}
-
-func (device *IpcDevice) RecoverDevice(gb28181Device gb28181_server.GB28181Device) {
-	global.Db.Debug().Where(
-		&IpcDevice{
-			DeviceID: gb28181Device.DeviceID,
-		},
-	).Updates(
-		&IpcDevice{
-			GB28181Device: gb28181Device,
-			DeviceID:      gb28181Device.DeviceID,
-		},
-	)
 }
 
 func (device *IpcDevice) UpdateChannels(channels []gb28181_server.GB28181Channel) {
